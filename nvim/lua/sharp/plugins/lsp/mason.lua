e7dc2b576a4020a57c954a04abcae99f6a3590ec
8ana4nam1ni0n <24194029+8ana4nam1ni0n@users.noreply.github.com>
2024-04-04 03:06:49 +0000

    Added the following:
        - Nvim plugins:
            - conform.nvim
            - nvim-lint
        - stylua formatter configuration
        - zellij autocompletion fixed on zsh

--- nvim/lua/sharp/plugins/lsp/mason.lua
+++ nvim/lua/sharp/plugins/lsp/mason.lua
@@ -1,28 +1,39 @@
 return {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     dependencies = {
-        'williamboman/mason-lspconfig.nvim',
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
-        local mason = require('mason')
-        local mason_lspconfig = require('mason-lspconfig')
-        local mason_tool_installer = require('mason-tool-installer')
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
         mason.setup({})
         mason_lspconfig.setup({
             ensure_installed = {
-                'lua_ls',
-                'dockerls',
-                'gopls',
-                'rust_analyzer',
-                'marksman',
-                'pyright',
-                'pylsp',
-                'hls',
+                "lua_ls",
+                "dockerls",
+                "gopls",
+                "rust_analyzer",
+                "marksman",
+                "pyright",
+                "pylsp",
             },
             automatic_installation = true,
         })
-        mason_tool_installer.setup({})
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "stylua",
+                "black",
+                "prettierd",
+                "goimports",
+                "isort",
+                "ruff",
+                "eslint_d",
+                "golangci-lint",
+            },
+        })
     end,
 }
