225d6300200e605a49dccfbbd2b699e50dbc2ac4
Yoshuam Alicea <sharp@yoshuams-mbp.lan>
2024-06-27 23:45:07 -0400

    New nvim config from arch added

---
+++ nvim/lua/sharp/plugins/git-worktree.lua
@@ -0,0 +1,8 @@
+return {
+    "ThePrimeagen/git-worktree.nvim",
+    config = function()
+        require("git-worktree").setup()
+
+        require("telescope").load_extension("git_worktree")
+    end,
+}
